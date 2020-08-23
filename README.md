3082fc6c78409707a61cb0ba613d13a4a34e3065
Musab <b_musab@hotmail.com>
2020-08-23 15:36:15 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,12 @@
+http://localhost:8080/getAll
+
+http://localhost:8080/create?firstName=Jack&lastName=Owen&age=25
+
+http://localhost:8080/create?firstName=Tom&lastName=Owen&age=25
+
+http://localhost:8080/get?firstName=Tom
+
+http://localhost:8080/update?firstName=Tom&lastName=Sparrow&age=30
+
+http://localhost:8080/delete?firstName=Tom
+
---
+++ src/main/java/com/mongodb/demo/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package com.mongodb.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.demo.model.Person;
+
+@Repository
+public interface PersonRepository extends MongoRepository<Person, String> {
+    public Person findByFirstName(String firstName);
+
+    public List<Person> findByAge(int age);
+}
